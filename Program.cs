903344896cb0c71468bc56be2007f6fccb18a5de
Cix8 <francesco.natal_fn@libero.it>
2022-09-26 10:40:19 +0200

    milestone 1: done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,151 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Event pastEvent = new Event();
+Event testEvent = new Event("test evento", "26-9-2022 12:00", 50);
 
+testEvent.ReserveSeats(48);
+
+
+try
+{
+    pastEvent.ReserveSeats(2);
+} catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+try
+{
+    testEvent.UnreserveSeats(50);
+} catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+testEvent.UnreserveSeats(48);
+
+Console.WriteLine(testEvent.ToString());
 public class Event
 {
-    public string Title { get; set; }
-    public DateTime Date { get; set; }
-    public int MaxCapacity { get; private set; }
+    protected string title;
+    protected DateTime date;
+    protected int maxCapacity;
     public int ReservedSeats { get; private set; }
+
+    public Event()
+    {
+        this.title = "evento passato";
+        this.date = DateTime.Parse("22-09-2022 15:00");
+        this.maxCapacity = 30;
+        this.ReservedSeats = 0;
+    }
+
+    public Event(string _title, string _date, int _maxCapacity)
+    {
+        this.Title = _title;
+        this.Date = DateTime.Parse(_date);
+        this.MaxCapacity = _maxCapacity;
+        this.ReservedSeats = 0;
+    }
+
+    public string Title
+    {
+        get
+        {
+            return this.title;
+        }
+        set
+        {
+            if (value.Trim().Length != 0)
+            {
+                this.title = value.Trim();
+            }
+            else
+            {
+                throw new Exception("Non è possibile salvare un evento senza titolo");
+            }
+        }
+    }
+
+    public DateTime Date
+    {
+        get
+        {
+            return this.date;
+        }
+        set
+        {
+            DateTime currentData = DateTime.Now;
+            DateTime eventData = value;
+            int result = DateTime.Compare(eventData, currentData);
+            if (result >= 0)
+            {
+                this.date = value;
+            }
+            else
+            {
+                throw new Exception("Data inserita non valida");
+            }
+        }
+    }
+
+    public int MaxCapacity
+    {
+        get
+        {
+            return this.maxCapacity;
+        }
+        private set
+        {
+            if (value > 0)
+            {
+                this.maxCapacity = value;
+            }
+            else
+            {
+                throw new Exception("La capacità massima non può essere negativa o uguale a zero");
+            }
+        }
+    }
+
+    public void ReserveSeats(int seats)
+    {
+        DateTime currentData = DateTime.Now;
+        DateTime eventData = this.Date;
+        int result = DateTime.Compare(eventData, currentData);
+        if (result < 0)
+        {
+            throw new Exception($"L'evento si è già tenuto in data {DateOnly.FromDateTime(this.Date)} alle ore {TimeOnly.FromDateTime(this.Date)}");
+        }
+        else if(this.MaxCapacity - this.ReservedSeats - seats >= 0)
+        {
+            this.ReservedSeats += seats;
+        } else
+        {
+            throw new Exception("Non posso prenotare il numero di posti inserito perchè supererebbe la capacità massima");
+        }
+    }
+
+    public void UnreserveSeats(int seats)
+    {
+        DateTime currentData = DateTime.Now;
+        DateTime eventData = this.Date;
+        int result = DateTime.Compare(eventData, currentData);
+        if (result < 0)
+        {
+            throw new Exception($"L'evento si è già tenuto in data {DateOnly.FromDateTime(this.Date)} alle ore {TimeOnly.FromDateTime(this.Date)}");
+        }
+        else if (this.ReservedSeats - seats >= 0)
+        {
+            this.ReservedSeats -= seats;
+        }
+        else
+        {
+            throw new Exception("I posti attualmente occupati sono inferiori al numero di prenotazioni che si intende disdire");
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Date.ToString("dd/MM/yyyy")} - {this.Title}";
+    }
 
 }
