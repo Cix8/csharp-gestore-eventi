e1d0985ac7aaa579a67dd20ed416d05712bd6166
Cix8 <francesco.natal_fn@libero.it>
2022-09-26 09:21:07 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Event
+{
+    public string Title { get; set; }
+    public DateTime Date { get; set; }
+    public int MaxCapacity { get; private set; }
+    public int ReservedSeats { get; private set; }
+
+}
